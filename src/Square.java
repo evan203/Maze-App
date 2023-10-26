dc736711a417dbf01dd3cd6b956bae19186b102c
evan203 <evancreeper203@gmail.com>
2023-10-26 13:41:23 -0500

    first commit in square branch

---
+++ src/Square.java
@@ -0,0 +1,20 @@
+public class Square {
+    int row;
+    int col;
+    int type;
+    public Square(int row, int col, int type)
+    {
+        this.row = row;
+        this.col = col;
+        this.type = type;
+
+    }
+    public String toString(){
+
+    }
+    public int getRow()
+    public int getCol()
+    public int getType()
+
+
+}
