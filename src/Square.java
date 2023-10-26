5caf09316270f391c154784c4eef848bf901206a
evan203 <evancreeper203@gmail.com>
2023-10-26 13:49:52 -0500

    complete square methods

--- src/Square.java
+++ src/Square.java
@@ -10,11 +10,29 @@ public class Square {
 
     }
     public String toString(){
-
+        switch (this.type)
+        {
+            case 0:
+                return "_";
+            case 1:
+                return "#";
+            case 2:
+                return "s";
+            case 3:
+                return "e";
+            case 4:
+                return "o"; // is on the solver work list
+            case 5:
+                return "."; // has been explored
+            case 6:
+                return "x"; // is on the final path to the exit
+            default:
+                return " ";
+        }
     }
-    public int getRow()
-    public int getCol()
-    public int getType()
+    public int getRow() {return this.row;}
+    public int getCol() {return this.col;}
+    public int getType() {return this.type;}
 
 
 }
