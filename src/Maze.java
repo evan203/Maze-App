d6218b50e47e422274d9ab5a111d5540ba619baf
Michael-Mihailov <111469137+Michael-Mihailov@users.noreply.github.com>
2023-10-27 14:09:03 -0500

    implemented loadMaze

--- src/Maze.java
+++ src/Maze.java
@@ -1,9 +1,56 @@
+import java.util.*;
+import java.io.*;
+
 class Maze
 {
     private Square[][]  maze;
 
     public Maze()
     {
+
+    }
+
+    boolean loadMaze(String fname)
+    {
+        File f = new File(fname);
+        Scanner scn = new Scanner(f);
+
+        String[] dimension = scn.nextLine().split(" ");
+
+        int numRows = Integer.parseInt(dimension[0]);
+        int numCols = Integer.parseInt(dimension[1]);
+
+        maze = new Square[numRows][numCols];
+
+        for (int row = 0; row < numRows; row++)
+        {            
+            String[] s = scn.nextLine().split(" ");
+            for (int col; col < numCols; col++)
+            {
+                maze[row][col] = new Square(row, col, Integer.parseInt(s[col]));
+            }
+        }
+    }
+
+    ArrayList<Square> getNeighbors(Square sq)
+    {
         
     }
+    Square getStart()
+    {
+
+    }
+    Square getFinish()
+    {
+
+    }
+    void reset()
+    {
+
+    }
+    String toString
+    {
+
+    }
+
 }
