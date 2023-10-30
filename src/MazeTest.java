d4e1e0ae59e0deb4b7eb85ab8a40dff610b8303e
Nick-Patel1 <105527171+Nick-Patel1@users.noreply.github.com>
2023-10-30 14:14:58 -0500

    testing maze

--- src/MazeTest.java
+++ src/MazeTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class MazeTest {
@@ -13,13 +14,22 @@ public class MazeTest {
         // Initialize a Square object before each test
         square = new Square(2, 3, 1);
         maze = new Maze();
+
     }
 
-    @Test
-    public void testLoadMaze() {
+    @Before
+    public void testLoadMaze() throws FileNotFoundException {
         String mazeFile = "src/maze-1";
+        boolean b;
+        try{
+            b = maze.loadMaze(mazeFile);
+        }
+        catch(FileNotFoundException e)
+        {
+            throw new FileNotFoundException();
+        }
         boolean expectedCondition = true;
-        assertEquals(expectedCondition, maze.loadMaze(mazeFile));
+        assertEquals(expectedCondition, b);
     }
 
     @Test
@@ -42,8 +52,27 @@ public class MazeTest {
     }
 
     @Test
-    public void testReset() {
-        maze.reset();
+    public void testReset() throws FileNotFoundException{
+        try{
+            maze.reset();
+        }
+        catch(FileNotFoundException e)
+        {
+            throw new FileNotFoundException();
+        }
+        // does the maze thats just been reset contain an characters that shouldn't be there?
+        // store the toString of maze into a string vraibale
+        String s = maze.toString();
+        Boolean b = false;
+        for (Character ch : s.toCharArray())
+            if (ch == 'o' || ch == '.' || ch == 'x')
+            {
+                    b = true;
+                    break;
+            }
+        
+
+        assertEquals(false, b);
     }
 
     @Test
