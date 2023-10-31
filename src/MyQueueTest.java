efadcb14bcda39ad8a964f21f0ad27d199610e79
Nick-Patel1 <105527171+Nick-Patel1@users.noreply.github.com>
2023-10-31 14:19:39 -0500

    implement queue for maze

---
+++ src/MyQueueTest.java
@@ -0,0 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class MyQueueTest 
+{
+    private MyQueue queue;
+
+    @Before
+    public void setUp() 
+    {
+        queue = new MyQueue();
+    }
+
+    @Test
+    public void testEnqueue() 
+    {
+        
+    }
+
+    @Test
+    public void testDequeue() throws NoSuchElementException
+    {
+        
+    }
+    
+    @Test
+    public void testFront() throws NoSuchElementException
+    {
+        
+    }
+
+    @Test
+    public void testSize() 
+    {
+        
+    }
+
+    @Test
+    public void testIsEmpty() 
+    {
+        
+    }
+
+    @Test
+    public void testClear() 
+    {
+        
+    }
+}
