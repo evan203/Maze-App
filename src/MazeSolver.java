42b96c54b519aa2b56d6565a6624e46d33d27e44
Nick-Patel1 <105527171+Nick-Patel1@users.noreply.github.com>
2023-11-03 14:14:07 -0500

    finished maze solver

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -23,30 +23,65 @@ public abstract class MazeSolver
         ArrayList<Square> neighbors = maze.getNeighbors(maze.getFinish());
         for (Square s : neighbors)
         {
-            if (s.getType() == 3)
+            if (s.getType() == 6)
             {
                 return true;
             }
         }
+
+        if (step() == null)
+        {
+            return true;
+        }
+
         return false;
     }
 
     public String getPath()
     {
-        if(isSolved())
+
+        Square n = null;
+
+        String path = "";
+        Square square = maze.getFinish();
+        for (Square s : maze.getNeighbors(square))
         {
-            String path;
-            
+            if (s.getType() == 1)
+            {
+                n = s;
+            }
         }
-        else 
+        if (n == null) return null;
+
+        while (n != null)
         {
-            return "No such Path";
+            // add n's coordinates to the path string
+            String cords = "[" + n.col + "," + n.row + "]";
+            path = cords + " " + path;
+
+            n = n.getPreviousSquare();
         }
+        
+        return "No such path";
     }
+
 
     public Square step()
     {
+        Square current = next();
+        current.type = 5;
 
+        ArrayList<Square> neighbors = maze.getNeighbors(current);
+        for (Square s : neighbors)
+        {
+            if (s.getType() == 0)
+            {
+                s.setPreviousSquare(current);
+                add(s);
+            }
+        }
+
+        return current;
     }
 
     public void solve()
